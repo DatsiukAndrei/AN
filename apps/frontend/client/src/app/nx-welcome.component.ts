d47b9aea8a7020ea7a5fb1dec1e938f1405254e7
DatsiukAndrei <datsiuk07031991@gmail.com>
2022-08-14 21:37:23 +0300

    initial commit

---
+++ apps/frontend/client/src/app/nx-welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'ad-nx-welcome',
+  template: `<div>Hello</div>`,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
